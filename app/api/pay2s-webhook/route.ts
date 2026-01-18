773ec1c073480ceeacdc10f2c89e6571d796890e
Hoang Duong <hrskai2001@gmail.com>
2026-01-18 16:33:44 +0700

    fix regex

--- app/api/pay2s-webhook/route.ts
+++ app/api/pay2s-webhook/route.ts
@@ -80,7 +80,6 @@ export async function POST(req: NextRequest) {
         where: {
           orderNumber: {
             contains: orderNumber.replace('ORD-', '').replace('ORD', ''),
-            mode: 'insensitive',
           },
           paymentStatus: 'PENDING',
         },
